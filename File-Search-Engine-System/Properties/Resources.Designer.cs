28437310b328b6556ae5fc50b22fc8a1ca4303b4
mennamohie <mennaxmohiex@gmail.com>
2018-05-01 17:31:19 +0200

    Semi-Final II.

--- File-Search-Engine-System/Properties/Resources.Designer.cs
+++ File-Search-Engine-System/Properties/Resources.Designer.cs
@@ -63,56 +63,6 @@ namespace File_Search_Engine_System.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap _36707202_894c_41d8_ba2e_a7b456413e58 {
-            get {
-                object obj = ResourceManager.GetObject("36707202-894c-41d8-ba2e-a7b456413e58", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _368f7a4c_dbc8_4062_94d9_67e3ade4c965 {
-            get {
-                object obj = ResourceManager.GetObject("368f7a4c-dbc8-4062-94d9-67e3ade4c965", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _462edab1_8bea_4bfd_9df4_8ff65d45f45d {
-            get {
-                object obj = ResourceManager.GetObject("462edab1-8bea-4bfd-9df4-8ff65d45f45d", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _954542d6_dd6d_428d_8792_d47e3e58182e {
-            get {
-                object obj = ResourceManager.GetObject("954542d6-dd6d-428d-8792-d47e3e58182e", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap b5fc9540_c160_4bde_9e63_b718732584f1 {
-            get {
-                object obj = ResourceManager.GetObject("b5fc9540-c160-4bde-9e63-b718732584f1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap file {
             get {
                 object obj = ResourceManager.GetObject("file", resourceCulture);
@@ -126,26 +76,6 @@ namespace File_Search_Engine_System.Properties {
         internal static System.Drawing.Bitmap folder {
             get {
                 object obj = ResourceManager.GetObject("folder", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap icon {
-            get {
-                object obj = ResourceManager.GetObject("icon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap icon__1_ {
-            get {
-                object obj = ResourceManager.GetObject("icon (1)", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
